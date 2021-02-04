2a99badc20d660e7d14a17cb3cf7498ae7071c23
Cristina Muñoz <hi@xmunoz.com>
2021-02-04 10:46:11 -0800

    Rename package to zalgo

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-from setuptools import setup
+import setuptools
 
-setup(
-    name="zalgo_text",
-    packages=["zalgo_text"],
+setuptools.setup(
+    name="zalgo",
     version="0.61",
     description="̖̘A ̩̼͜͡ś̛̺̦̃m̡̥̫a̛̳̹l̵̷͐ͪl ̶̫l̞̆̇i͈̫br̮̗͘a͇͗̌r̝ͨ͜͜y ̼̃ͅṯ̴̯́͒ͩo ̷̘ćͫr͕̎͆é͋a̛̳͈͗͜tͫͮe ̵̳͗̍͡z̴͇̗a̮̹ͬl̛͒̐ģ̏o ́͡ͅs̜͗̂t̙̬͟͢ý̶̹l̪ȩ͍͓̄̾͠d ̵̷̠̌t̸͈̖́̎͢e̸̓̓x̮ͮt.",
+    packages=setuptools.find_packages(),
     setup_requires=["wheel"],
     url="https://github.com/Cronenbrogues/zalgo",
     keywords=["diacritic", "filter", "zalgo", "he is coming", "ahhh"],
